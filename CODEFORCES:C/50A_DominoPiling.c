83b23c1e8ac119fc12c10c10d5b1969d2b0c7582
aybukeaksoy <66042648+aybukeaksoy@users.noreply.github.com>
2021-02-15 22:30:26 +0300

    Add files via upload

---
+++ CODEFORCES:C/50A_DominoPiling.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(int argc,char **argv){
+    #define MAX(x, y) (((x) > (y)) ? (x) : (y))
+    #define MIN(x, y) (((x) < (y)) ? (x) : (y))
+    int n;
+    int m;
+    scanf("%d %d",&n,&m);
+    if (m%2==0 || n%2==0){
+        printf("%d",m*n/2);
+    }
+    else{
+        printf("%d",MAX(m,n)/2*MIN(m,n)+MIN(m,n)/2);
+    }
+
+}
